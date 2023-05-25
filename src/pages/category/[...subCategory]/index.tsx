3e49430486efd0a36aaf0909c34a45e91b1a9c14
muhsin-life2 <128311823+muhsin-life2@users.noreply.github.com>
2023-05-25 15:15:51 +0400

    Update index.tsx

--- src/pages/category/[...subCategory]/index.tsx
+++ src/pages/category/[...subCategory]/index.tsx
@@ -1,19 +1,30 @@
 import { ProductsPage } from "@/components/products-page"
 import getProductsDataByCat from "@/lib/getProductsDataByCat"
 
-const subcategory = ({ params, categoryData }: { params: any, categoryData: any }) => {
-    return <ProductsPage type={params.subCategory[params.length - 1]} categoryData={categoryData} menuData={["Category", String(params.subCategory[params.subCategory.length - 1]).replace(/-/g, ' ')]} />
+const subcategory = ({ params, categoryData, filterPath, selectedBrands }: { params: any, categoryData: any, filterPath:any, selectedBrands:string }) => {
+    return <ProductsPage filterPath={filterPath} isSearchPage={false}  categoryData={categoryData} menuData={["Category", String(params.subCategory[params.subCategory.length - 1]).replace(/-/g, ' ')]} selectedBrands = { selectedBrands } />
 }
 
 export default subcategory
 
-export async function getServerSideProps({ locale, params }: { locale: any, params: any }) {
+export async function getServerSideProps({ locale, params, query }: { locale: any, params: any, query: any }) {
+    let filterPath = `categories`
+    const subCategory = params.subCategory[params.subCategory.length - 1]
+    if (subCategory) {
+        filterPath += `=${subCategory}`
+    }
+    if (query.brands) {
+        if (filterPath != "categories") {
+            filterPath += `&brands=${query.brands}`
+        }
+    }
 
-    const categoryData = await getProductsDataByCat("categories", params.subCategory[params.subCategory.length - 1], 0, false, locale);
+    const categoryData = await getProductsDataByCat(filterPath, 0, false, locale);
 
     return {
         props: {
             categoryData: categoryData.data,
+            filterPath,
             params
         },
     };
