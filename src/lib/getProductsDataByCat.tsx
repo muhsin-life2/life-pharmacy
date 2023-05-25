360bec19edb17642c3a8c419a1a2642f98f5aac7
muhsin-life2 <128311823+muhsin-life2@users.noreply.github.com>
2023-05-25 15:18:52 +0400

    Update getProductsDataByCat.tsx

--- src/lib/getProductsDataByCat.tsx
+++ src/lib/getProductsDataByCat.tsx
@@ -1,7 +1,7 @@
 
-export default async function getProductsDataByCat(cat:string, type:string, noOfProducts:number, isProductsPage:boolean, lang:any) {
+export default async function getProductsDataByCat(filterPath:string, noOfProducts:number, isProductsPage:boolean, lang:any) {
 
-    const urlPath =`https://prodapp.lifepharmacy.com/api/web/products?${isProductsPage ? "" : cat != "" ? `${cat}=${type}&` : ""}order_by=popularity&type=cols&skip=${noOfProducts}&take=40&new_method=true&lang=${lang}`;
+    const urlPath =`https://prodapp.lifepharmacy.com/api/web/products?${isProductsPage ? "" : `${filterPath}&` }order_by=popularity&type=cols&skip=${noOfProducts}&take=40&new_method=true&lang=${lang}`;
     console.log(urlPath);
     
     const res = await fetch(urlPath)
