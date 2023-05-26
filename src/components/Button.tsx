23df0f7d852d8da1159174bcd063a7e502c7e640
muhsin-life2 <128311823+muhsin-life2@users.noreply.github.com>
2023-05-26 18:10:07 +0400

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 
-export const BrandsButton = ({ selectedBrands, brandName }: { selectedBrands: string, brandName: any }) => {
+export const BrandsButton = ({ selectedBrands, brandName }: { selectedBrands: string, brandName: any}) => {
     const router = useRouter()
 
     const [isInverted, setIsInverted] = useState(false);
@@ -9,23 +9,20 @@ export const BrandsButton = ({ selectedBrands, brandName }: { selectedBrands: st
 
     const brandsOnClick = (clickedBrand: any) => {
         if (selectedBrands.includes(clickedBrand)) {
-            router.push(router.asPath.replace(`${brandsArray.length === 1 ? "&brands=" + clickedBrand : "," + clickedBrand}`, ""))
+            return router.asPath.replace(`${brandsArray.length === 1 ? "&brands=" + clickedBrand : "," + clickedBrand}`, "")
         }
         else {
             if (router.query.brands) {
-                router.push(`${router.asPath},${clickedBrand}`)
+                return `${router.asPath},${clickedBrand}`
             }
             else {
-                router.push(`${router.asPath}&brands=${clickedBrand}`)
+                return `${router.asPath}&brands=${clickedBrand}`
             }
         }
-        setIsInverted(!isInverted)
     }
 
     return (
-        <div onClick={() => {
-            brandsOnClick(brandName.toLowerCase().replace(/[\s&]+/g, '-'))
-        }} className={`${brandsArray.includes(brandName.toLowerCase().replace(/[\s&]+/g, '-')) ? "!bg-blue-500 !text-white" : ""} ${isInverted ? "!bg-blue-500 !text-white " : " "} cursor-pointer text-blue-500 border border-blue-500 px-2 py-1 text-center my-1 mr-2 rounded-full hover:bg-blue-500 hover:text-white inline-block text-xs`}>{brandName}</div>
+        <a href={brandsOnClick(brandName.toLowerCase().replace(/[\s&]+/g, '-'))} onClick={()=>{setIsInverted(!isInverted)}} className={`${brandsArray.includes(brandName.toLowerCase().replace(/[\s&]+/g, '-')) ? "!bg-blue-500 !text-white" : ""} ${isInverted ? "!bg-blue-500 !text-white " : " "} cursor-pointer text-blue-500 border border-blue-500 px-2 py-1 text-center my-1 mr-2 rounded-full hover:bg-blue-500 hover:text-white inline-block text-xs`}>{brandName}</a>
 
     )
 }
