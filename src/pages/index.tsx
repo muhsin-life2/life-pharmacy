daf4079fc503d3b721f5aca6e07f2ebb0854f1c2
muhsin-life2 <128311823+muhsin-life2@users.noreply.github.com>
2023-05-30 13:45:09 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,12 @@ import Image from 'next/image'
 import PageStructure from '@/components/page-structure';
 import Products from '@/components/products';
 import { useLanguage } from '@/hooks/useLanguage';
-export default function Home({ homePageData, locale }: { homePageData: any, locale:any }) {
+export default function Home({ homePageData, locale }: { homePageData: any, locale: any }) {
   return (
-
-    
-        homePageData.data.content.map((data: any, ind: number) => (
-          <PageStructure data={data} lang={locale} setLoading={ind === 0 ? true : false}>
-            <Products lang={locale} slug={data.section_data_object?.slug} type_key={data.section_data_object?.type_key} />
-          </PageStructure>))
-  
+    homePageData.data.content.map((data: any, ind: number) => (
+      <PageStructure data={data} lang={locale} setLoading={ind === 0 ? true : false}>
+        <Products lang={locale} slug={data.section_data_object?.slug} type_key={data.section_data_object?.type_key} />
+      </PageStructure>))
   )
 }
 
