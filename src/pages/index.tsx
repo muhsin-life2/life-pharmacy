da85b68ca57603f634c57853571240b93e07a182
muhsin-life2 <128311823+muhsin-life2@users.noreply.github.com>
2023-05-30 14:55:53 +0530

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,13 @@ import Products from '@/components/products';
 import { useLanguage } from '@/hooks/useLanguage';
 export default function Home({ homePageData, locale }: { homePageData: any, locale:any }) {
   return (
-    <div className='max-w-[1450px] px-[10px] mx-auto'>
-      {
+
+    
         homePageData.data.content.map((data: any, ind: number) => (
           <PageStructure data={data} lang={locale} setLoading={ind === 0 ? true : false}>
             <Products lang={locale} slug={data.section_data_object?.slug} type_key={data.section_data_object?.type_key} />
           </PageStructure>))
-      }
-    </div>
+  
   )
 }
 
